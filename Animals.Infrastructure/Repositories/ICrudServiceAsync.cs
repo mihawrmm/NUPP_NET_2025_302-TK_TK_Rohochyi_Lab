13e5a0cdfcef5f36df11a264a00b13e0a2fdc7ee
mihawrmm <misha.rogo4y@gmail.com>
2025-04-26 21:29:24 +0300

    Лабораторна 3

---
+++ Animals.Infrastructure/Repositories/ICrudServiceAsync.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals.Infrastructure.Repositories
+{
+    public interface ICrudServiceAsync<T> where T : class
+    {
+        Task<bool> CreateAsync(T element);
+        Task<T> ReadAsync(int id);
+        Task<IEnumerable<T>> ReadAllAsync();
+        Task<IEnumerable<T>> ReadAllAsync(int page, int amount);
+        Task<bool> UpdateAsync(T element);
+        Task<bool> RemoveAsync(T element);
+        Task<bool> SaveAsync();
+    }
+}
